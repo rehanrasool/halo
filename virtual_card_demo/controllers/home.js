ec8cf771f5a7fdde5232b2ef16a0952c51ff1ce0
Rehan Rasool <rehan.rasool9@gmail.com>
2019-03-08 18:11:06 -0500

    virtual_card_demo: create user, then create card product and then create a card for that user

--- virtual_card_demo/controllers/home.js
+++ virtual_card_demo/controllers/home.js
@@ -1,5 +1,7 @@
 var request = require('request');
 
+var parent="c2e632ad-ab41-42b8-9d9e-9091e170aa4d";
+
 /**
  * GET /
  * Home page.
@@ -10,37 +12,111 @@ exports.index = (req, res) => {
   });
 };
 
+
 /**
  * POST /sendCard
  * Send a contact form via Nodemailer.
  */
 exports.sendCard = (req, res) => {
   console.log("send halo card!");
+  console.log(req.body);
 
-  var request = require('request');
+  // -------------------------- USER INPUT ---------------------------- //
+  var amount = req.body.amount;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var birthdate = req.body.birthdate;
+  var address = req.body.address;
+  var city = req.body.city;
+  var state = req.body.state;
 
   var headers = {
       'accept': 'application/json',
+      'Content-Type': 'application/json',
       'Authorization': 'Basic dXNlcjU0OTYxNTQ4NTQ5MDg1OmI4ZTI2YWViLTg3Y2QtNDI1ZS05NzI1LTI5ODU0MzY3YmU2NA=='
   };
 
-  var options = {
-      url: 'https://shared-sandbox-api.marqeta.com/v3/cards/1234',
-      headers: headers
+  // -------------------------- CREATE USER ---------------------------- //
+  var userData = '{"parent_token":"'+parent+'","first_name":"'+firstname+'","last_name":"'+lastname+'","email":"'+email+'","birth_date":"'+birthdate+'","address1":"'+address+'","city":"'+city+'","state":"'+state+'","country":"US"}';
+
+  var userOptions = {
+      url: 'https://shared-sandbox-api.marqeta.com/v3/users/',
+      method: 'POST',
+      headers: headers,
+      body: userData
   };
 
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log("> success");
+  // -------------------------- CREATE CARD PRODUCT ---------------------------- //
+
+  var cardProductName = "halo-card-product-1";
+  var cardProductStartDate = "2019-01-01";
+  var cardProductData = '{"name":"'+cardProductName+'","start_date":"'+cardProductStartDate+'"}';
+
+  var cardProductOptions = {
+      url: 'https://shared-sandbox-api.marqeta.com/v3/cardproducts/',
+      method: 'POST',
+      headers: headers,
+      body: cardProductData
+  };
+
+  // -------------------------- CREATE CARD ---------------------------- //
+
+  var cardUserToken; // filled through callback
+  var cardProductToken; // filled through callback
+
+  var cardOptions = {
+      url: 'https://shared-sandbox-api.marqeta.com/v3/cards/',
+      method: 'POST',
+      headers: headers,
+      // body: cardData
+  };
+
+  function createCardCallback(error, response, body) {
+      if (!error && response.statusCode == 201) {
+        console.log("##### card: success");
         console.log(body);
       } else {
-        console.log("> failure");
+        console.log("##### card: failure");
         console.log(error);
         console.log(response);
       }
   }
 
-  request(options, callback);
+  function createCardProductCallback(error, response, body) {
+      if (!error && response.statusCode == 201) {
+        console.log("##### card product: success");
+        console.log(body);
+        data = JSON.parse(body);
+        cardProductToken = data['token'];
+
+        // create card for user
+        var cardData = '{"user_token":"'+cardUserToken+'","card_product_token":"'+cardProductToken+'"}';
+        cardOptions['body']=cardData;
+        request(cardOptions, createCardCallback);
+      } else {
+        console.log("##### card product: failure");
+        console.log(error);
+        console.log(response);
+      }
+  }
+
+  function createUserCallback(error, response, body) {
+      if (!error && response.statusCode == 201) {
+        console.log(body);
+        data = JSON.parse(body);
+        cardUserToken = data['token'];
+
+        // create card product
+        request(cardProductOptions, createCardProductCallback);
+      } else {
+        console.log("##### user: failure");
+        console.log(error);
+        console.log(response);
+      }
+  }
+
+  request(userOptions, createUserCallback); // create user
 
   return res.redirect('/');
 };
