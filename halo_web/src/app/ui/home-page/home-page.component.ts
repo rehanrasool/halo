02ffe3e99e212c44762b9e483da70651f404b9f4
Rehan Rasool <rehan.rasool9@gmail.com>
2019-04-23 01:58:02 -0400

    halo_web: beautify the news feed

--- halo_web/src/app/ui/home-page/home-page.component.ts
+++ halo_web/src/app/ui/home-page/home-page.component.ts
@@ -27,10 +27,38 @@ export class HomePageComponent implements OnInit {
       map((actions) => {
         return actions.map((a) => {
           const data = a.payload.doc.data();
-          data.timestamp=data.timestamp.toDate(); // convert from firebase format
+          data.timestamp=timeSince(data.timestamp.toDate()); // convert from firebase format
           return { id: a.payload.doc.id, ...data };
         });
       })
     );
   }
+}
+
+function timeSince(date) {
+
+  var seconds = Math.floor((+new Date() - date) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
 }
